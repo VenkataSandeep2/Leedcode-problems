45e3eec58696855837bb2fa5237b2bd5cb03e3b0
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-03-15 23:37:40 +0530

    Add string reversal functionality in Reversestring.java
    Implement string reversal and print length.

---
+++ Reversestring.java
@@ -0,0 +1,13 @@
+class Reversestring
+{
+public static void main(String args[])
+	{
+	String s = "Sanjay" ;
+	System.out.println(s.length());
+	for(int i = 5 ;i>= 0 ; i--)
+	 	{
+		//System.out.print(i);		It's prints index numbers
+		System.out.print(s.charAt(i));
+		}
+	}
+}
