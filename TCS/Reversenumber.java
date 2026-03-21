b052330c5ead209cd01d248f6fa9bbcc8a1f0c12
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-03-21 12:04:30 +0530

    Create Reversenumber.java

---
+++ TCS/Reversenumber.java
@@ -0,0 +1,15 @@
+public class Reversenumber {
+	public static void main(String[] args) {
+		String s = "1234";
+		String ans = "";
+		{
+			for (int i =s.length()-1;i>=0;i--)
+			{
+				ans = ans + s.charAt(i);
+			}
+			System.out.println(ans);
+		}
+
+	}
+
+}
