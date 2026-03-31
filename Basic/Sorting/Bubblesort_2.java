1bc09b3d5567ee425c16b91319b8e733e5480f4b
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-03-31 22:50:26 +0530

    Create Bubblesort_2.java

---
+++ Basic/Sorting/Bubblesort_2.java
@@ -0,0 +1,25 @@
+class Bubblesort_2
+{
+  public static void main(String args[])
+  {
+    int[] arr={8,5,3,1,4};
+    int n = arr.length;
+
+    for (int i = 0; i<n;i++)
+      {
+        for(int j = 0;j<n-i-1;j++)
+          {
+            if(arr[j] > arr[j+1])
+               {
+              int temp = arr[j];
+              arr[j]=arr[j+1];
+                arr[j+1] = temp;
+               }
+          }
+      }
+    for(int i = 0;i<n;i++)
+      {
+        System.out.print(arr[i] +" ");
+      }
+  }
+}
