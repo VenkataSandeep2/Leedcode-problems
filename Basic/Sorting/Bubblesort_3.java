ab84a5b749fa9e17cc74464abbefbe9e9bc2c7c4
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-04-01 19:53:41 +0530

    Implement Bubble Sort algorithm in Bubblesort_3.java

---
+++ Basic/Sorting/Bubblesort_3.java
@@ -0,0 +1,27 @@
+public class Bubblesort_3 
+{
+	public static void main(String args[])
+	{
+		int[] arr = {8,22,4,24,1,5};
+		int n =arr.length;
+		
+		for(int i = 0; i<n; i++)
+		{
+			for(int j = 0; j<n-i-1; j++)
+			{
+				if(arr[j] > arr[j+1])
+				{
+					int temp =arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+					
+				}
+			}
+		}
+	for(int i = 0;i<n;i++)
+	{
+		System.out.print(arr[i] +" ");
+	}
+	}
+
+}
